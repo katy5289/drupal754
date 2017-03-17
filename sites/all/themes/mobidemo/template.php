c223fdc6bed70243bade6c7fd39d50ff3129fbd7
Katy5289 <katy@seascapewebdesign.com>
2017-03-17 10:27:34 -0700

    adding modules

---
+++ sites/all/themes/mobidemo/template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "footheme" to match
+ * your subthemes name, e.g. if you name your theme "elimostest" then the function
+ * name will be "elimostest_preprocess_hook". Tip - you can search/replace
+ * on "footheme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function elimostest_preprocess_html(&$vars) {
+}
+function elimostest_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function elimostest_preprocess_page(&$vars) {
+}
+function elimostest_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function elimostest_preprocess_node(&$vars) {
+}
+function elimostest_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function elimostest_preprocess_comment(&$vars) {
+}
+function elimostest_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function elimostest_preprocess_block(&$vars) {
+}
+function elimostest_process_block(&$vars) {
+}
+// */
